f722bb0041ea7ddb3f33c000e5a53527f5450409
FepDev25 <lamediatricolor@gmail.com>
2025-07-09 18:35:48 -0500

    Agregando base de datos corregido

--- db/db.sql
+++ db/db.sql
@@ -20,24 +20,29 @@ CREATE TABLE usuario (
     contrasena VARCHAR(100) NOT NULL
 );
 
+
 CREATE TABLE cuenta (
     id SERIAL PRIMARY KEY,
     usuario_id INT NOT NULL,
     saldo NUMERIC(12, 2) NOT NULL DEFAULT 0.00,
     fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    estado_id INT NOT NULL,
 
-    CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id) ON DELETE CASCADE
+    CONSTRAINT fk_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id) ON DELETE CASCADE,
+    CONSTRAINT fk_estado FOREIGN KEY (estado_id) REFERENCES estado_cuenta (id)
 );
+
 
 CREATE TABLE transaccion (
     id SERIAL PRIMARY KEY,
     cuenta_origen_id BIGINT,
     cuenta_destino_id BIGINT,
     monto NUMERIC(12, 2) NOT NULL,
-    tipo VARCHAR(20) NOT NULL,
+    tipo_id INT NOT NULL,
     fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_cuenta_origen FOREIGN KEY (cuenta_origen_id) REFERENCES cuenta (id) ON DELETE SET NULL,
-    CONSTRAINT fk_cuenta_destino FOREIGN KEY (cuenta_destino_id) REFERENCES cuenta (id) ON DELETE SET NULL
+    CONSTRAINT fk_cuenta_destino FOREIGN KEY (cuenta_destino_id) REFERENCES cuenta (id) ON DELETE SET NULL,
+    CONSTRAINT fk_tipo FOREIGN KEY (tipo_id) REFERENCES tipo_transaccion (id)
 );
 
