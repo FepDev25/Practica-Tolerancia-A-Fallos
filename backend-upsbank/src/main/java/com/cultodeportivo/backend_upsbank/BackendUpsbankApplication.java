c1a27f9cbf05d34efd2c4aff6950db3f2cffb798
FepDev25 <lamediatricolor@gmail.com>
2025-07-09 18:02:38 -0500

    Creacion de proyectos

---
+++ backend-upsbank/src/main/java/com/cultodeportivo/backend_upsbank/BackendUpsbankApplication.java
@@ -0,0 +1,13 @@
+package com.cultodeportivo.backend_upsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUpsbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUpsbankApplication.class, args);
+	}
+
+}
---
+++ backend-upsbank/src/test/java/com/cultodeportivo/backend_upsbank/BackendUpsbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultodeportivo.backend_upsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUpsbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
