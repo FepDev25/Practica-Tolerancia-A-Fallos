d030b5d5afca20834809e530fbe4614127a2e518
FepDev25 <lamediatricolor@gmail.com>
2025-07-15 12:32:10 -0500

    Fix backend problems

--- backend-upsbank/src/main/java/com/cultodeportivo/backend_upsbank/SecurityConfig.java
+++ backend-upsbank/src/main/java/com/cultodeportivo/backend_upsbank/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
